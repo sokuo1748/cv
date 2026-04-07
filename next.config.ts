e4f99cb7248a71162b6599e66d337f49e20bec27
sokuo <sokuo1748@gmail.com>
2026-04-07 10:16:03 +0800

    fixed some next/image setting

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/sokuo-cv',
   assetPrefix: '/sokuo-cv/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
