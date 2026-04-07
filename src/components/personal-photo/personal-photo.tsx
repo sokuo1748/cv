89d1bd51e7aba653cba02047276d87a612e39726
sokuo <sokuo1748@gmail.com>
2026-04-07 10:53:02 +0800

    fixed some next/image error

--- src/components/personal-photo/personal-photo.tsx
+++ src/components/personal-photo/personal-photo.tsx
@@ -6,10 +6,10 @@ export function PersonalPhoto() {
     <div className={styles.main}>
       <div className={styles.photoWrapper}>
         <Image
-          src="/assets/images/person-photo/photo.JPG"
-          alt="陳暐龍"
-          fill
-        />
+  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/assets/images/person-photo/photo.JPG`}
+  alt="陳暐龍"
+  fill
+/>
       </div>
       <strong className={styles.name}>陳暐龍</strong>
       <span className={styles.divider} />
